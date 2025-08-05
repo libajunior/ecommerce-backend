fddd13f9ee78b6c6808a88e2f1e668a6c6c01544
Libório Júnior <libajunior@gmail.com>
2025-08-04 21:47:21 -0300

    feat: Conexão com o banco de dados (aula 04/08)

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'aws-0-sa-east-1.pooler.supabase.com',
+      port: +'5432',
+      username: 'postgres.zriqiljmquzhjussbqde',
+      password: 'postgres',
+      database: 'postgres',
+    }),
+  ],
+})
+export class AppModule {}
