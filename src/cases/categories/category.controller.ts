01666d8fcba4c9f42cc47dbabe8ae4a56fd9c972
Libório Júnior <libajunior@gmail.com>
2025-08-18 16:48:39 -0300

    feat: Implementação da classes de entidade e service

---
+++ src/cases/categories/category.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, ParseUUIDPipe } from "@nestjs/common";
+import { Category } from "./category.entity";
+import { CategoryService } from "./category.service";
+
+@Controller('categories')
+export class CategoryController {
+
+  constructor(private readonly service: CategoryService) {}
+
+  @Get()
+  findAll(): Promise<Category[]> {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id', ParseUUIDPipe) id: string): Promise<Category> {
+
+  }
+
+}
+
